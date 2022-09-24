f7d891d9b99fd204e8aa312afa18b78de5836b04
Alexandre Mutel <alexandre_mutel@live.com>
2022-09-24 15:02:41 +0200

    Update file headers

--- src/DrayTekWatcher/Connectors/TelnetClient.cs
+++ src/DrayTekWatcher/Connectors/TelnetClient.cs
@@ -1,4 +1,8 @@
-﻿namespace DrayTekWatcher.Core.Connectors;
+﻿// Copyright (c) Alexandre Mutel. All rights reserved.
+// Licensed under the BSD-Clause 2 license.
+// See license.txt file in the project root for full license information.
+
+namespace DrayTekWatcher.Core.Connectors;
 
 using System.Diagnostics;
 using System.Net.Sockets;
--- src/draytek-watcher/Program.cs
+++ src/draytek-watcher/Program.cs
@@ -1,6 +1,8 @@
-﻿ // See https://aka.ms/new-console-template for more information
+﻿// Copyright (c) Alexandre Mutel. All rights reserved.
+// Licensed under the BSD-Clause 2 license.
+// See license.txt file in the project root for full license information.
 
- using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration;
  using Microsoft.Extensions.Configuration.CommandLine;
  using Microsoft.Extensions.DependencyInjection;
  using Microsoft.Extensions.Hosting;
